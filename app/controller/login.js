d0b3147f657de412cf20b15bf7e2fccb046cd907
Souta <im@souta.cc>
2025-10-02 02:04:15 +0800

    feat: 完成统一身份认证登录

--- app/controller/login.js
+++ app/controller/login.js
@@ -27,14 +27,14 @@ class LoginController extends Controller {
     }
 
     // 获取验证码图片
-    const captchaImage = this.getCaptchaImage(ctx, session);
+    const captchaImage = await this.getCaptchaImage(ctx, session);
     if (!captchaImage) {
       ctx.body = { code: 500, message: "Internal Server Error" };
       return;
     }
 
     // 获取验证码识别文本
-    const captchaText = this.getCaptchaText(ctx, captchaImage);
+    const captchaText = await this.getCaptchaText(ctx, captchaImage);
     if (!captchaText) {
       ctx.body = { code: 500, message: "Internal Server Error" };
       return;
@@ -49,12 +49,38 @@ class LoginController extends Controller {
       ctx.body = { code: 500, message: "Internal Server Error" };
       return;
     }
+
+    // 统一身份认证登录
+    const { status: authStatus, ticket: authTicket } = await this.authLogin(
+      ctx,
+      session,
+      username,
+      encryptedPassword,
+      captchaText,
+      execution
+    );
+    console.log(authStatus);
+    if (authStatus === 1) {
+      ctx.body = { code: 400, message: "Bad Request" };
+      return;
+    } else if (authStatus === 2) {
+      ctx.body = { code: 401, message: "Unauthorized" };
+      return;
+    } else if (authStatus === 3) {
+      ctx.body = { code: 403, message: "Forbbiden" };
+      return;
+    }
+
+    if (!authTicket) {
+      ctx.body = { code: 500, message: "Internal Server Error" };
+      return;
+    }
   }
 
   /**
    * 获取 JESSIONID, Execution, Salt
    * @param {Egg.Context<any>} ctx Context
-   * @returns
+   * @returns {{session: string|null, execution: string|null, salt: string|null}} JSESSIONID, Execution, Salt
    */
   async getKeys(ctx) {
     // 发起请求
@@ -150,6 +176,69 @@ class LoginController extends Controller {
 
     // 返回验证码文本
     return res.data.data;
+  }
+
+  /**
+   * 统一身份认证登录
+   * @param {Egg.Context<any>} ctx Context
+   * @param {string} session JSESSIONID
+   * @param {string} username 用户名
+   * @param {string} password 密码
+   * @param {string} captcha 验证码文本
+   * @param {string} execution Execution
+   * @returns {{status: number, ticket: string|null}} 登录状态，登录凭证
+   */
+  async authLogin(ctx, session, username, password, captcha, execution) {
+    // 发起请求
+    const res = await ctx.curl(
+      `${ctx.app.config.auth.base}${ctx.app.config.auth.url.login}`,
+      {
+        method: "POST",
+        headers: {
+          Cookie: "JSESSIONID=" + session,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: {
+          username,
+          password,
+          captcha,
+          _eventId: "submit",
+          cllt: "userNameLogin",
+          dllt: "generalLogin",
+          lt: "",
+          execution,
+        },
+      }
+    );
+
+    // 登录成功
+    if (res.status === 302) {
+      const ticket = res.headers["set-cookie"]
+        .map((c) => c.split(";")[0])
+        .find((c) => c.startsWith("CASTGC="))
+        ?.split("=")[1];
+      return { status: 0, ticket };
+    }
+
+    // 获取错误提示
+    const htmlBody = res.data.toString();
+    const matchMessage = htmlBody.match(
+      /<span id="showErrorTip"[^>]*>\s*<span[^>]*>(.*?)<\/span>\s*<\/span>/
+    );
+    const message = matchMessage ? matchMessage[1] : null;
+
+    // 验证码错误
+    if (message && message.includes("图形动态码错误")) {
+      return { status: 1, ticket: null };
+    }
+
+    // 用户名或密码错误
+    if (message && message.includes("用户名或者密码有误")) {
+      return { status: 2, ticket: null };
+    }
+
+    // 其他错误
+    return { status: 3, ticket: null };
   }
 }
 
