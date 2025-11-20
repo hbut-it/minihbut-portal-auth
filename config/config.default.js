9ece3640455676bc40e51d8cdfe56ae521fae663
Souta <im@souta.cc>
2025-11-21 01:22:39 +0800

    chore: 更新 OCR 接口配置的基础 URL

--- config/config.default.js
+++ config/config.default.js
@@ -31,7 +31,7 @@ module.exports = (appInfo) => {
 
   // OCR 接口配置
   config.ocr = {
-    base: "http://127.0.0.1:8000",
+    base: "http://ddddocr-fastapi:8000",
     url: {
       ocr: "/ocr",
     },
